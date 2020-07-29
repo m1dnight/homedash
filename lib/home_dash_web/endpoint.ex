9fc90bb0fa9b08438fc21849d986a149da7f519e
Christophe De Troyer <christophe.detroyer@gmail.com>
2020-07-29 22:10:28 +0200

    fix live socket

--- lib/home_dash_web/endpoint.ex
+++ lib/home_dash_web/endpoint.ex
@@ -14,6 +14,8 @@ defmodule HomeDashWeb.Endpoint do
     websocket: true,
     longpoll: false
 
+  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
@@ -46,10 +48,7 @@ defmodule HomeDashWeb.Endpoint do
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-    store: :cookie,
-    key: "_home_dash_key",
-    signing_salt: "tmInhqbd"
+  plug Plug.Session, @session_options
 
   plug HomeDashWeb.Router
 end
