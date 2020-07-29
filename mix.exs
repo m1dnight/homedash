a4d86fce5ce4ef5d958dfd7a26e3631af482f8d0
Christophe De Troyer <christophe.detroyer@gmail.com>
2020-07-29 23:11:44 +0200

    bump elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule HomeDash.MixProject do
     [
       app: :home_dash,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
