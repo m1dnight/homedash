a95083c8b729ec49ffe198184cf07c72a44e4422
Christophe De Troyer <christophe.detroyer@gmail.com>
2020-07-30 15:54:26 +0200

    format input json for posting data

--- lib/home_dash_web/controllers/datapoint_controller.ex
+++ lib/home_dash_web/controllers/datapoint_controller.ex
@@ -10,16 +10,26 @@ defmodule HomeDashWeb.DataPointController do
     render(conn, "current.json", current)
   end
 
-  def create_electricity_datapoint(conn, %{"value" => _value} = params) do
+  def create_electricity_datapoint(conn, %{"value" => value, "read_on" => ro}) do
+    {:ok, ro, 0} = DateTime.from_iso8601(ro)
+    params = %{"value" => value, "read_on" => ro}
+
     with {:ok, %ElectricityDataPoint{}} <- DataPoints.create_electricity_data_point(params) do
       conn
       # what does this do? :)
       |> put_status(:created)
       |> send_resp(200, "OK")
+    else
+      _ ->
+        conn
+        |> send_resp(500, "Invalid input given.")
     end
   end
 
-  def create_gas_datapoint(conn, %{"value" => _value} = params) do
+  def create_gas_datapoint(conn,  %{"value" => value, "read_on" => ro}) do
+    {:ok, ro, 0} = DateTime.from_iso8601(ro) # 0 offset because we want UTC!
+    params = %{"value" => value, "read_on" => ro}
+
     with {:ok, %GasDataPoint{}} <- DataPoints.create_gas_data_point(params) do
       conn
       # what does this do? :)
