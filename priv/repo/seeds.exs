b6e6fdb5fc564cabb01ee8f3b833d57609eaf7c9
Christophe De Troyer <christophe.detroyer@gmail.com>
2020-08-05 14:27:54 +0200

    delete seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -18,19 +18,19 @@ alias HomeDash.DataPoints
 # DataPoints.create_gas_data_point(%{value: 0, read_on: now})
 # DataPoints.create_solar_data_point(%{value: 0, read_on: now})
 
-0..10
-|> Enum.flat_map(fn day ->
-  1..3
-  |> Enum.map(fn hour ->
-    date =
-      DateTime.now!("Etc/UTC")
-      |> DateTime.add(-1 * day * 24 * 60 * 60, :second, Calendar.get_time_zone_database())
-      |> DateTime.add(hour * 60 * 60, :second, Calendar.get_time_zone_database())
+# 0..10
+# |> Enum.flat_map(fn day ->
+#   1..3
+#   |> Enum.map(fn hour ->
+#     date =
+#       DateTime.now!("Etc/UTC")
+#       |> DateTime.add(-1 * day * 24 * 60 * 60, :second, Calendar.get_time_zone_database())
+#       |> DateTime.add(hour * 60 * 60, :second, Calendar.get_time_zone_database())
 
-    IO.puts(date)
+#     IO.puts(date)
 
-    DataPoints.create_electricity_data_point(%{value: day * 100 + hour * 10, read_on: date})
-    DataPoints.create_gas_data_point(%{value: 1000 + day * 100 + hour * 100, read_on: date})
-    DataPoints.create_solar_data_point(%{value: 2000 + day * 100 + hour * 1000, read_on: date})
-  end)
-end)
+#     DataPoints.create_electricity_data_point(%{value: day * 100 + hour * 10, read_on: date})
+#     DataPoints.create_gas_data_point(%{value: 1000 + day * 100 + hour * 100, read_on: date})
+#     DataPoints.create_solar_data_point(%{value: 2000 + day * 100 + hour * 1000, read_on: date})
+#   end)
+# end)
